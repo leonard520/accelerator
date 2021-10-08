9e4a49375d457940743326f2bd393c8c8af12270
Xiaoyun Ding <xiading@microsoft.com>
2021-10-08 22:03:11 +0800

    fix

--- src/main/java/io/cloudevents/examples/springboot/Application.java
+++ src/main/java/io/cloudevents/examples/springboot/Application.java
@@ -17,15 +17,51 @@
 
 package io.cloudevents.examples.springboot;
 
+import io.cloudevents.CloudEvent;
+import io.cloudevents.spring.webflux.CloudEventHttpMessageReader;
+import io.cloudevents.spring.webflux.CloudEventHttpMessageWriter;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.codec.CodecCustomizer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.CodecConfigurer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+@RestController
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    private static List<String> messages = new LinkedList<>();
+
+    @GetMapping("/sns")
+    public String echo() {
+        return Arrays.toString(messages.toArray());
+    }
+
+    @PostMapping("/")
+    public Mono<String> event(@RequestBody Mono<CloudEvent> inputEvent) {
+        Mono<String> vaule = inputEvent.map(event -> event.toString()).doOnSuccess(v -> messages.add(v));
+        return vaule;
+    }
+
+    @Configuration
+    public static class CloudEventHandlerConfiguration implements CodecCustomizer {
+
+        @Override
+        public void customize(CodecConfigurer configurer) {
+            configurer.customCodecs().register(new CloudEventHttpMessageReader());
+            configurer.customCodecs().register(new CloudEventHttpMessageWriter());
+        }
+    }
 }
